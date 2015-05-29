218b54aa6e4d789eb259215482db954808c83b07
pendo324 <pendo3247@gmail.com>
2015-05-28 22:40:31 -0400

    Forgot to add the cases

--- OBS Now Playing/SourceManager.cs
+++ OBS Now Playing/SourceManager.cs
@@ -28,6 +28,20 @@ namespace OBS_Now_Playing
                     sh = new iTunesHandler(path, preview);
                     Task itunes = sh.pollForSongChanges();
                     break;
+                case "MPC-HC":
+                    sh = new MPCHandler(path, preview);
+                    Task mpc = sh.pollForSongChanges();
+                    break;
+                case "VLC":
+                    sh = new VLCHandler(path, preview);
+                    Task vlc = sh.pollForSongChanges();
+                    break;
+                case "WinAmp":
+                    sh = new WinAmpHandler(path, preview);
+                    Task winamp = sh.pollForSongChanges();
+                    break;
+                case "YouTube":
+                    break;
                 default:
                     break;
             }
