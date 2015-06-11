e45591e43fc0bb7a786633a292ef1d0a327150b9
pendo324 <pendo3247@gmail.com>
2015-06-11 01:17:05 -0400

    I think this is it

--- OBS Now Playing/nowplaying.js
+++ OBS Now Playing/nowplaying.js
@@ -163,7 +163,7 @@ require(['jquery'], function () {
             contentType: "application/json; charset=utf-8",
             url: url,
             dataType: "json",
-            data: { 'song': song.title },
+            data: { 'song': song.song },
             success: function (data) {
             }
         });
